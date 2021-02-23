8c9bfba70ef891c965f819c3c3d9a2938b35ff9e
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-02-23 19:43:17 +0100

    Funcion de si es palindroma o no funcionando

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 const frasePalindroma = "Amigo no gima";
 const otraFrasePalindroma = "A Mercedes ése de crema";
+const fraseNoPalidroma = "Hola como estas";
 
-const eliminarAccentos = frase => frase.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+const eliminarAccentosYEspacios = frase => frase.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[.,:;()_?¿!¡-\s]/g, "");
 
 const esPalindroma = frase => {
-  const fraseSinMayusculasYAccentos = eliminarAccentos(frase).toLowerCase();
-  console.log(fraseSinMayusculasYAccentos);
-  const fraseReverse = "";
-  return frase === fraseReverse;
+  const fraseSinMayusculasYAccentos = eliminarAccentosYEspacios(frase).toLowerCase();
+  const fraseInvertida = fraseSinMayusculasYAccentos.split("").reverse().join("");
+  return fraseSinMayusculasYAccentos === fraseInvertida;
 };
 
-esPalindroma(otraFrasePalindroma);
+console.log(`La frase '${frasePalindroma}'${esPalindroma(frasePalindroma) ? "" : " no"} es palindroma`);
+console.log(`La frase '${otraFrasePalindroma}'${esPalindroma(otraFrasePalindroma) ? "" : " no"} es palindroma`);
+console.log(`La frase '${fraseNoPalidroma}'${esPalindroma(fraseNoPalidroma) ? "" : " no"} es palindroma`);
